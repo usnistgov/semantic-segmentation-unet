7d3390e99167a14018b5f2333ae4c932187294ee
Michael Majurski <michael.majurski@nist.gov>
2019-05-10 12:23:29 -0400

    added image file format options

--- UNet/inference.py
+++ UNet/inference.py
@@ -27,6 +27,7 @@ def load_model(checkpoint_filepath, gpu_id, number_classes):
 
         # build list of variables to restore
         vars = tf.global_variables()
+        print('Loading variables:')
         for v in vars:
             print('{}   {}'.format(v._shared_name, v.shape))
 
@@ -56,7 +57,7 @@ def _inference(img_filepath, sess, input_op, logits_op):
         print('image height needs to be a multiple of {}, padding with reflect'.format(factor))
     if img.shape[1] % factor != 0:
         pad_x = (factor - img.shape[1] % factor)
-        print('image width needs to be a multiple of {}}, padding with reflect'.format(factor))
+        print('image width needs to be a multiple of {}, padding with reflect'.format(factor))
     if pad_x > 0 or pad_y > 0:
         img = np.pad(img, pad_width=((0, pad_y), (0, pad_x)), mode='reflect')
 
@@ -73,30 +74,7 @@ def _inference(img_filepath, sess, input_op, logits_op):
     return pred
 
 
-def main():
-    parser = argparse.ArgumentParser(prog='inference', description='Script to detect stars with the selected unet model')
-
-    parser.add_argument('--gpu', dest='gpu_id', type=int, help='which gpu to use for training (can only use a single gpu)', default=0)
-    parser.add_argument('--checkpoint_filepath', dest='checkpoint_filepath', type=str, help='Checkpoint filepath to the  model to use', required=True)
-    parser.add_argument('--image_folder', dest='image_folder', type=str, help='filepath to the folder containing tif images to inference (Required)', required=True)
-    parser.add_argument('--output_folder', dest='output_folder', type=str, required=True)
-    parser.add_argument('--number_classes', dest='number_classes', type=int, default=2)
-
-    args = parser.parse_args()
-
-    gpu_id = args.gpu_id
-    checkpoint_filepath = args.checkpoint_filepath
-    image_folder = args.image_folder
-    output_folder = args.output_folder
-    number_classes = args.number_classes
-
-    print('Arguments:')
-    print('number_classes = {}'.format(number_classes))
-    print('gpu_id = {}'.format(gpu_id))
-    print('checkpoint_filepath = {}'.format(checkpoint_filepath))
-    print('image_folder = {}'.format(image_folder))
-    print('output_folder = {}'.format(output_folder))
-
+def inference(gpu_id, checkpoint_filepath, image_folder, output_folder, number_classes, image_format):
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # so the IDs match nvidia-smi
     if gpu_id != -1:
         os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
@@ -105,7 +83,7 @@ def main():
     if not os.path.exists(output_folder):
         os.mkdir(output_folder)
 
-    img_filepath_list = [os.path.join(image_folder, fn) for fn in os.listdir(image_folder) if fn.endswith('.tif')]
+    img_filepath_list = [os.path.join(image_folder, fn) for fn in os.listdir(image_folder) if fn.endswith('.{}'.format(image_format))]
 
     sess, input_op, logits_op = load_model(checkpoint_filepath, gpu_id, number_classes)
 
@@ -116,6 +94,7 @@ def main():
         print('{}/{} : {}'.format(i, len(img_filepath_list), slide_name))
 
         segmented_mask = _inference(img_filepath, sess, input_op, logits_op)
+
         if 0 <= np.max(segmented_mask) <= 255:
             segmented_mask = segmented_mask.astype(np.uint8)
         if 255 < np.max(segmented_mask) < 65536:
@@ -126,4 +105,35 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog='inference',
+                                     description='Script to detect stars with the selected unet model')
+
+    parser.add_argument('--gpu', dest='gpu_id', type=int,
+                        help='which gpu to use for training (can only use a single gpu)', default=0)
+    parser.add_argument('--checkpoint_filepath', dest='checkpoint_filepath', type=str,
+                        help='Checkpoint filepath to the  model to use', required=True)
+    parser.add_argument('--image_folder', dest='image_folder', type=str,
+                        help='filepath to the folder containing tif images to inference (Required)', required=True)
+    parser.add_argument('--output_folder', dest='output_folder', type=str, required=True)
+    parser.add_argument('--number_classes', dest='number_classes', type=int, default=2)
+    parser.add_argument('--image_format', dest='image_format', type=str, help='format (extension) of the input images. E.g {tif, jpg, png)', default='tif')
+
+    args = parser.parse_args()
+
+    gpu_id = args.gpu_id
+    checkpoint_filepath = args.checkpoint_filepath
+    image_folder = args.image_folder
+    output_folder = args.output_folder
+    number_classes = args.number_classes
+    image_format = args.image_format
+
+    print('Arguments:')
+    print('number_classes = {}'.format(number_classes))
+    print('gpu_id = {}'.format(gpu_id))
+    print('checkpoint_filepath = {}'.format(checkpoint_filepath))
+    print('image_folder = {}'.format(image_folder))
+    print('output_folder = {}'.format(output_folder))
+    print('image_format = {}'.format(image_format))
+
+    inference(gpu_id, checkpoint_filepath, image_folder, output_folder, number_classes, image_format)
+
