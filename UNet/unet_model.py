ab2d5437705b29c183330904dc8da52333f4ec9d
Michael Majurski <michael.majurski@nist.gov>
2019-10-15 21:28:17 -0400

    removed merge issue

--- UNet/unet_model.py
+++ UNet/unet_model.py
@@ -8,120 +8,6 @@ if sys.version_info[0] < 3:
 
 import tensorflow as tf
 tf_version = tf.__version__.split('.')
-<<<<<<< HEAD
-if int(tf_version[0]) != 1 or int(tf_version[1]) != 12:
-    import warnings
-    warnings.warn('Codebase only tested using Tensorflow version 1.12.x')
-
-NUMBER_CHANNELS = 1  # grayscale
-
-BASELINE_FEATURE_DEPTH = 64
-
-# If a model is trained with multiple GPUs, prefix all Op names with tower_name
-# to differentiate the operations. Note that this prefix is removed from the
-# names of the summaries when visualizing a model.
-TOWER_NAME = 'tower'
-
-
-def conv_layer(layer_in, fc_out, kernel, is_training, stride=1, name=None):
-    conv_output = tf.layers.conv2d(
-        layer_in,
-        filters=fc_out,
-        kernel_size=kernel,
-        padding='same',
-        activation=tf.nn.relu,
-        strides=stride,
-        data_format='channels_first',  # translates to NCHW
-        kernel_initializer=tf.contrib.layers.xavier_initializer(),
-        name=name)
-    conv_output = tf.layers.batch_normalization(conv_output, training=is_training, axis=1)  # axis to normalize (the channels dimension)
-    return conv_output
-
-
-def dropout(layer_in, is_training, drop_rate=0.5, name=None):
-
-    #use tf.layers.dropout function
-    drop_output = tf.layers.dropout(
-        layer_in,
-        rate=drop_rate,
-        training=is_training,
-        name=name)
-    return drop_output
-
-def deconv_layer(layer_in, fc_out, kernel, is_training, stride=1, name=None):
-    conv_output = tf.layers.conv2d_transpose(
-        layer_in,
-        filters=fc_out,
-        kernel_size=kernel,
-        padding='same',
-        activation=None,
-        strides=stride,
-        data_format='channels_first',  # translates to NCHW
-        kernel_initializer=tf.contrib.layers.xavier_initializer(),
-        name=name)
-    conv_output = tf.layers.batch_normalization(conv_output, training=is_training, axis=1)  # axis to normalize (the channels dimension)
-    return conv_output
-
-
-def add_inference_ops(inputs, is_training, number_classes=2):
-    kernel = 3
-    deconv_kernel = 2
-    pooling_stride = 2
-
-    with tf.variable_scope('unet'):
-        with tf.variable_scope("encoder"):
-            conv_1 = conv_layer(inputs, BASELINE_FEATURE_DEPTH, kernel, is_training)
-            conv_1 = conv_layer(conv_1, BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            pool_1 = tf.layers.max_pooling2d(conv_1, pool_size=pooling_stride, strides=pooling_stride, data_format='channels_first')
-
-            conv_2 = conv_layer(pool_1, 2 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            conv_2 = conv_layer(conv_2, 2 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            pool_2 = tf.layers.max_pooling2d(conv_2, pool_size=pooling_stride, strides=pooling_stride, data_format='channels_first')
-
-            conv_3 = conv_layer(pool_2, 4 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            conv_3 = conv_layer(conv_3, 4 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            pool_3 = tf.layers.max_pooling2d(conv_3, pool_size=pooling_stride, strides=pooling_stride, data_format='channels_first')
-
-            conv_4 = conv_layer(pool_3, 8 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            conv_4 = conv_layer(conv_4, 8 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            conv_4 = dropout(conv_4, is_training)
-
-            pool_4 = tf.layers.max_pooling2d(conv_4, pool_size=pooling_stride, strides=pooling_stride, data_format='channels_first')
-
-            # bottleneck
-            bottleneck = conv_layer(pool_4, 16 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            bottleneck = conv_layer(bottleneck, 16 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            bottleneck = dropout(bottleneck, is_training)
-
-        with tf.variable_scope("decoder"):
-            # up-conv which reduces the number of feature channels by 2
-            deconv_4 = deconv_layer(bottleneck, 8 * BASELINE_FEATURE_DEPTH, deconv_kernel, is_training, stride=pooling_stride)
-            deconv_4 = tf.concat([conv_4, deconv_4], axis=1)
-            deconv_4 = conv_layer(deconv_4, 8 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            deconv_4 = conv_layer(deconv_4, 8 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            deconv_3 = deconv_layer(deconv_4, 4 * BASELINE_FEATURE_DEPTH, deconv_kernel, is_training, stride=pooling_stride)
-            deconv_3 = tf.concat([conv_3, deconv_3], axis=1)
-            deconv_3 = conv_layer(deconv_3, 4 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            deconv_3 = conv_layer(deconv_3, 4 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            deconv_2 = deconv_layer(deconv_3, 2 * BASELINE_FEATURE_DEPTH, deconv_kernel, is_training, stride=pooling_stride)
-            deconv_2 = tf.concat([conv_2, deconv_2], axis=1)
-            deconv_2 = conv_layer(deconv_2, 2 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-            deconv_2 = conv_layer(deconv_2, 2 * BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-            deconv_1 = deconv_layer(deconv_2, BASELINE_FEATURE_DEPTH, deconv_kernel, is_training, stride=pooling_stride)
-            deconv_1 = tf.concat([conv_1, deconv_1], axis=1)
-            deconv_1 = conv_layer(deconv_1, BASELINE_FEATURE_DEPTH, kernel, is_training)
-            deconv_1 = conv_layer(deconv_1, BASELINE_FEATURE_DEPTH, kernel, is_training)
-
-        logits = conv_layer(deconv_1, number_classes, 1, is_training, name='logits') # 1x1 kernel to convert feature map into class map
-=======
 if int(tf_version[0]) != 2:
     raise Exception('Tensorflow 2.x.x required')
 
@@ -240,7 +126,7 @@ class UNet():
         deconv_1 = UNet._conv_layer(deconv_1, UNet._BASELINE_FEATURE_DEPTH, UNet._KERNEL_SIZE)
 
         logits = UNet._conv_layer(deconv_1, self.number_classes, 1)  # 1x1 kernel to convert feature map into class map
->>>>>>> tf2.0b
+
         # convert NCHW to NHWC so that softmax axis is the last dimension
         logits = tf.keras.layers.Permute((2, 3, 1))(logits)
         # logits is [NHWC]
