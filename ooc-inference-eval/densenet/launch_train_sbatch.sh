76af968a386397cf524927f86dded34aa8791f74
Michael Majurski <michael.majurski@nist.gov>
2020-02-25 16:10:34 -0500

    launch scripts

--- ooc-inference-eval/densenet/launch_train_sbatch.sh
+++ ooc-inference-eval/densenet/launch_train_sbatch.sh
@@ -7,9 +7,9 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=160
 #SBATCH --gres=gpu:4
-#SBATCH --job-name=unet
-#SBATCH -o unet_%N.%j.out
-#SBATCH --time=24:0:0
+#SBATCH --job-name=fcd
+#SBATCH -o fcd_%N.%j.out
+#SBATCH --time=48:0:0
 
 # NIST-developed software is provided by NIST as a public service. You may use, copy and distribute copies of the software in any medium, provided that you keep intact this entire notice. You may improve, modify and create derivative works of the software or any portion of the software, and you may copy and distribute such modifications or works. Modified works should carry a notice stating that you changed the software and should note the date and nature of any such change. Please explicitly acknowledge the National Institute of Standards and Technology as the source of the software.
 # NIST-developed software is expressly provided "AS IS." NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY, RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
@@ -18,18 +18,18 @@
 
 # job configuration
 test_every_n_steps=1000
-batch_size=8 # 4x across the gpus
+batch_size=4 # 4x across the gpus
 
-train_lmdb_file="train-hes-500.lmdb"
+train_lmdb_file="train-hes.lmdb"
 test_lmdb_file="test-hes.lmdb"
 
-input_data_directory="/wrk/mmajursk/small-data-cnns/data"
-output_directory="/wrk/mmajursk/tmp"
+input_data_directory="/wrk/mmajursk/ooc/"
+output_directory="/wrk/mmajursk/ooc/"
 
-experiment_name="unet-$(date +%Y%m%dT%H%M%S)"
+experiment_name="fcd-$(date +%Y%m%dT%H%M%S)"
 
 number_classes=2
-learning_rate=3e-4
+learning_rate=1e-4
 use_augmentation=1
 balance_classes=1
 
@@ -46,6 +46,7 @@ scratch_dir="/scratch/${SLURM_JOB_ID}"
 term_handler()
 {
         echo "function term_handler called.  Cleaning up and Exiting"
+        rm -rf ${scratch_dir}/*
         # Do nothing
         exit -1
 }
@@ -54,7 +55,7 @@ term_handler()
 trap 'term_handler' TERM
 
 source /opt/anaconda3/etc/profile.d/conda.sh
-conda activate tf2
+conda activate ooc
 
 # make working directory
 mkdir -p ${scratch_dir}
@@ -78,6 +79,7 @@ cp -r . "$results_dir/src"
 # launch training script with required options
 echo "Launching Training Script"
 
-python train_unet.py --test_every_n_steps=${test_every_n_steps} --batch_size=${batch_size} --train_database="$scratch_dir/$train_lmdb_file" --test_database="$scratch_dir/$test_lmdb_file" --output_dir="$results_dir" --number_classes=${number_classes} --learning_rate=${learning_rate}  --use_augmentation=${use_augmentation} --balance_classes=${balance_classes} | tee "$results_dir/log.txt"
+python train_fcd.py --test_every_n_steps=${test_every_n_steps} --batch_size=${batch_size} --train_database="$scratch_dir/$train_lmdb_file" --test_database="$scratch_dir/$test_lmdb_file" --output_dir="$results_dir" --number_classes=${number_classes} --learning_rate=${learning_rate}  --use_augmentation=${use_augmentation} --balance_classes=${balance_classes}
 
+rm -rf ${scratch_dir}/*
 echo "Job completed"
